be03e6b5f8ea04457fdcf9159bab92ebb6d81ccb
M'BAYE Ba Aly <baaly.mbaye@yahoo.fr>
2024-10-19 13:54:07 +0200

    [TDD] Mise en place test d'integration depot sur un compte

---
+++ src/main/java/fr/sg/bankaccountkata/interfaces/controllers/AccountController.java
@@ -0,0 +1,18 @@
+package fr.sg.bankaccountkata.interfaces.controllers;
+
+import fr.sg.bankaccountkata.usecases.account.AccountService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.security.auth.login.AccountNotFoundException;
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+}
