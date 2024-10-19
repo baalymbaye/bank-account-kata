8a4ee5507a4e55a4512b8b7f515c94b638d76973
M'BAYE Ba Aly <baaly.mbaye@yahoo.fr>
2024-10-19 13:55:40 +0200

    Mise en place service + methode de depot

---
+++ src/main/java/fr/sg/bankaccountkata/usecases/account/AccountService.java
@@ -0,0 +1,15 @@
+package fr.sg.bankaccountkata.usecases.account;
+
+import javax.security.auth.login.AccountNotFoundException;
+import java.math.BigDecimal;
+
+public interface AccountService {
+    /**
+     * Dépose un montant sur le compte spécifié.
+     *
+     * @param accountId L'identifiant unique du compte.
+     * @param amount Le montant à déposer.
+     * @throws AccountNotFoundException Si le compte spécifié n'existe pas.
+     */
+    void deposit(String accountId, BigDecimal amount) throws AccountNotFoundException;
+}
