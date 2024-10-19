cb69fc2c3941ffc7259badd3f8527e65d45427e3
M'BAYE Ba Aly <baaly.mbaye@yahoo.fr>
2024-10-19 14:24:22 +0200

    [US3] Feature permettant d'afficher l'historique des transactions

--- src/main/java/fr/sg/bankaccountkata/interfaces/controllers/AccountController.java
+++ src/main/java/fr/sg/bankaccountkata/interfaces/controllers/AccountController.java
@@ -1,10 +1,13 @@
 package fr.sg.bankaccountkata.interfaces.controllers;
 
+import fr.sg.bankaccountkata.domain.Transaction;
 import fr.sg.bankaccountkata.usecases.account.AccountService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.security.auth.login.AccountNotFoundException;
 import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -23,5 +26,15 @@ public class AccountController {
     @PostMapping("/{accountId}/withdraw")
     public void withdraw(@PathVariable String accountId, @RequestParam BigDecimal amount) throws AccountNotFoundException {
         accountService.withdraw(accountId, amount);
+    }
+
+    @GetMapping("/{accountId}/statement")
+    public ResponseEntity<String> printStatement(@PathVariable String accountId) throws AccountNotFoundException {
+        return ResponseEntity.ok(accountService.printStatement(accountId));
+    }
+
+    @GetMapping("/{accountId}/history")
+    public ResponseEntity<List<Transaction>> getHistory(@PathVariable String accountId) throws AccountNotFoundException {
+        return ResponseEntity.ok(accountService.getHistory(accountId));
     }
 }
--- src/main/java/fr/sg/bankaccountkata/usecases/account/AccountService.java
+++ src/main/java/fr/sg/bankaccountkata/usecases/account/AccountService.java
@@ -1,7 +1,10 @@
 package fr.sg.bankaccountkata.usecases.account;
 
+import fr.sg.bankaccountkata.domain.Transaction;
+
 import javax.security.auth.login.AccountNotFoundException;
 import java.math.BigDecimal;
+import java.util.List;
 
 public interface AccountService {
     /**
@@ -21,4 +24,22 @@ public interface AccountService {
      * @throws AccountNotFoundException Si le compte spécifié n'existe pas.
      */
     void withdraw(String accountId, BigDecimal amount) throws AccountNotFoundException;
+
+    /**
+     * Récupère l'historique des transactions pour le compte spécifié.
+     *
+     * @param accountId L'identifiant unique du compte.
+     * @return Une liste d'objets TransactionDto représentant l'historique des transactions.
+     * @throws AccountNotFoundException Si le compte spécifié n'existe pas.
+     */
+    List<Transaction> getHistory(String accountId) throws AccountNotFoundException;
+
+    /**
+     * Génère un relevé de compte sous forme de chaîne de caractères.
+     *
+     * @param accountId L'identifiant unique du compte.
+     * @return Une chaîne de caractères représentant le relevé de compte.
+     * @throws AccountNotFoundException Si le compte spécifié n'existe pas.
+     */
+    String printStatement(String accountId) throws AccountNotFoundException;
 }
